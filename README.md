dd44e325220ee14977553598bc4926a5ff6ad245
Yutong Zhang <54720958+Yutong-Z@users.noreply.github.com>
2022-09-30 21:27:32 +0000

    script to dowanload kaggle data

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # IDS706-Project2-Yutong
 This is the repo for IDS706 course project 2.
+
+### Usage
+
+#### Getting access to Kaggle dataset
+
+If you see OSError: Could not find kaggle.json. Make sure it's located in /home/vscode/.kaggle. Or use the environment method.. Please download Kaggle token with your account and mv it to required dirtectory.
+
+#### Downloading Kaggle dataset
+
+`./download_kaggle_data.sh <kaggle_data_name>`
+
+Example: `./download_kaggle_data.sh store-sales-time-series-forecasting`
---
+++ download_kaggle_data.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: ./download_kaggle_data.sh -d <parameter>"
+   echo -e "\t-d The name of the Kaggle dataset to download."
+   exit 1 # exit script after printing help
+}
+
+# retrieve dataset name as parameter
+while getopts "d:" opt
+do
+   case "$opt" in
+      d ) KAGGLE_DATA="$OPTARG" ;;
+      ? ) helpFunction ;; # print helpFunction in case parameter is non-existent
+   esac
+done
+
+# print helpFunction in case parameter is empty
+if [ -z "$KAGGLE_DATA" ]
+then
+   echo "The parameter is empty!";
+   helpFunction
+fi
+
+# prepare environment
+# sudo apt install unzip
+make install
+sudo ln -s ~/.local/bin/kaggle /usr/bin/kaggle
+
+# dowanload data
+echo "First arg: $KAGGLE_DATA"
+# KAGGLE_DATA=store-sales-time-series-forecasting
+kaggle competitions download -c $KAGGLE_DATA
+
+# unzip data
+rm -rf data
+mkdir data
+unzip $KAGGLE_DATA.zip -d data
