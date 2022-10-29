959593f4f584cb0c87f8a6b584bacb7fddea3c19
Yutong Zhang <54720958+Yutong-Z@users.noreply.github.com>
2022-10-29 00:26:26 +0000

    add queries using where/group/order

--- query_nyc_jobs.py
+++ query_nyc_jobs.py
@@ -10,9 +10,9 @@ import dotenv
 import os
 
 dotenv.load_dotenv()
-TOKEN=os.getenv('TOKEN')
-USERNAME=os.getenv('USERNAME')
-PASSWORD=os.getenv('PASSWORD')
+TOKEN = os.getenv('TOKEN')
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
 
 
 # Unauthenticated client only works with public data sets. Note 'None'
@@ -22,14 +22,28 @@ PASSWORD=os.getenv('PASSWORD')
 # Example authenticated client (needed for non-public datasets):
 client = Socrata("data.cityofnewyork.us",
                  TOKEN,
-                 username=USERNAME,
-                 password=PASSWORD)
+                 username = USERNAME,
+                 password = PASSWORD)
 
 # First 2000 results, returned as JSON from API / converted to Python list of
 # dictionaries by sodapy.
-results = client.get("kpav-sd4t", limit=2000, select="job_id, agency")
+results = client.get("kpav-sd4t", limit = 2000, select = "*")
 
 # Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
+print(results_df)
+
+select_clause = "job_id, agency, posting_date, salary_range_from, salary_range_to"
+where_clause = "job_category='Technology, Data & Innovation' AND career_level='Entry-Level'"
+order_by_clause = "posting_date DESC"
+results = client.get("kpav-sd4t", limit = 10, select = select_clause, where = where_clause, order = order_by_clause)
+results_df = pd.DataFrame.from_records(results)
+print(results_df)
+
+select_clause = "job_category, COUNT(job_id)"
+group_by_clause = "job_category"
+order_by_clause = "COUNT(job_id) DESC"
+results = client.get("kpav-sd4t", limit = 10, select = select_clause, group = group_by_clause, order = order_by_clause)
 results_df = pd.DataFrame.from_records(results)
 print(results_df)
 
